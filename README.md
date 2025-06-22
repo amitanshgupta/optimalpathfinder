aa12c6ba89468db5c2a056c802db4e4cc063fa79
amitanshgupta <amitanshg1@gmail.com>
2025-06-22 19:21:59 +0000

    Done

--- README.md
+++ README.md
@@ -1,1 +1,124 @@
-# optimalpathfinder
+# Optimal Path Finder
+[![Build Status](https://travis-ci.org/urastogi885/optimal-path-finder.svg?branch=master)](https://travis-ci.org/github/urastogi885/optimal-path-finder)
+[![License](https://img.shields.io/badge/License-MIT-blue.svg)](https://github.com/urastogi885/optimal-path-finder/blob/master/LICENSE)
+
+## Overview
+
+This project implements search-based algorithms that always yield an optimal path if one exists. Path finding 
+algorithms, such as breadth-first search (BFS), Dijkstra, and A*, have been used find an optimal path for the robot 
+from the user-defined start to end point. The project also generates animation to visualize the exploration of each of 
+the mentioned search-based methods. It first checks that the user inputs do not lie in the obstacle space. Note that 
+the obstacle space is pre-defined and static.
+
+<p align="center">
+  <img src="https://github.com/urastogi885/optimal-path-finder/blob/master/images/rigid_robot_exploration.gif">
+  <br><b>Figure 1 - Node exploration for a rigid robot using A*</b><br>
+</p>
+
+The project has been improved from its previous [release](https://github.com/urastogi885/optimal-path-finder/releases). 
+The exploration time from one corner of the obstacle space to the other has been reduced to just less than a second from
+several minutes. Upon running, exploration and animation time are printed on the execution window.
+
+Note that the implementation in this repository assumes the robot to be holonomic. I have used the A* implementation from 
+this projects as a base to find path for a non-holonomic robot in the following projects: [
+A-star Robot](https://github.com/urastogi885/a-star-robot) and 
+[A-star Turtlebot](https://github.com/urastogi885/a-star-turtlebot).
+
+## Dependencies
+
+- Python3
+- Python3-tk
+- Python3 Libraries: Numpy, OpenCV-Python
+
+## Install Dependencies
+
+- Install Python3, Python3-tk, and the necessary libraries: (if not already installed)
+````
+sudo apt install python3 python3-tk
+sudo apt install python3-pip
+pip3 install numpy opencv-python
+````
+
+- Check if your system successfully installed all the dependencies
+- Open terminal using Ctrl+Alt+T and enter python3
+- The terminal should now present a new area represented by >>> to enter python commands
+- Now use the following commands to check libraries: (Exit python window using Ctrl+Z if an error pops up while
+running the below commands)
+````
+import tkinter
+import numpy
+import cv2
+````
+
+## Run
+
+- Using the terminal, clone this repository and go into the project directory, and run the main program:
+````
+git clone https://github.com/urastogi885/optimal-path-finder
+cd optimal-path-finder
+````
+
+- If you have a compressed version of the project, extract it, go into project directory, open the terminal, and run
+the robot explorer.
+````
+python3 robot_explorer.py <start_x,start_y> <goal_x,goal_y> <robot_radius> <clearance> <method> <animation>
+````
+- You can use the following one-letter acronyms to specify the method:
+    - `b` - For Breadth-First Search (BFS)
+    - `d` - For Dijkstra
+    - `a` - For A*
+- An example for using BFS for a point robot:
+````
+python3 robot_explorer.py 5,5 295,195 0 0 b 1
+````
+<p align="center">
+  <img src="https://github.com/urastogi885/optimal-path-finder/blob/master/images/exploration_b.gif">
+  <br><b>Figure 2 - Exploration + Path using BFS using above start and goal points</b><br>
+</p>
+
+````
+python3 robot_explorer.py <start_x,start_y> <goal_x,goal_y> <robot_radius> <clearance> <method> <animation>
+python3 robot_explorer.py 5,5 295,195 0 0 d 1
+````
+- Radius and clearance for a point robot should be set as `0`
+- Animation parameter is to display exploration and path. Use `1` to show animation.
+
+<p align="center">
+  <img src="https://github.com/urastogi885/optimal-path-finder/blob/master/images/point_explorer.gif">
+  <br><b>Figure 3 - Exploration for point robot using Djikstra's algorithm</b><br>
+</p>
+
+- To run the rigid robot version, after execution of the previous command or open a new terminal from the project
+folder:
+````
+python3 robot_explorer.py <start_x,start_y> <goal_x,goal_y> <robot_radius> <clearance> <method> <animation>
+python3 robot_explorer.py 5,5 295,195 1 1 d 1
+````
+
+<p align="center">
+  <img src="https://github.com/urastogi885/optimal-path-finder/blob/master/images/rigid_explorer.png">
+  <br><b>Figure 4 - Final path for rigid robot</b><br>
+</p>
+
+- A* algorithm has also been implemented in the project and can be run by using `a` as the method parameter instead of `d`
+````
+python3 robot_explorer.py <start_x,start_y> <goal_x,goal_y> <robot_radius> <clearance> <method> <animation>
+python3 robot_explorer.py 5,5 295,195 1 1 a 1
+````
+
+<p align="center">
+  <img src="https://github.com/urastogi885/optimal-path-finder/blob/master/images/a_star_rigid.png">
+  <br><b>Figure 5 - Final path for rigid robot using A*</b><br>
+</p>
+
+
+## Notes
+
+- Both the explorers, point and rigid, take first 2 arguments as the start and goal points. The x,y values for each
+point are separated by a comma. DO NOT INCLUDE ANY SPACE AFTER THE COMMAS
+- There should a space after each argument.
+- The rigid explorer takes 2 extra arguments: robot radius and clearance. Please refer the format using the instance
+provided in the Run section.
+- The explorer first finds a path from the start to the goal, then starts displaying the exploration of the map, and
+the final path.
+- The map for the rigid robot is generated such that shows the extended obstacles due to the robot radius and clearance.
---
+++ utils/explorer.py
@@ -0,0 +1,156 @@
+# Import necessary standard libraries
+import cv2
+import numpy as np
+from math import sqrt
+from queue import PriorityQueue
+# Import custom-built methods
+from utils import constants
+from utils.actions import take_action
+
+
+def check_node_validity(check_img: np.ndarray, x: int, y: int) -> bool:
+    """
+    Method to check whether point lies within any obstacle
+    :param check_img: 2-d array with information of the map
+    :param x: x-coordinate of the current node
+    :param y: y-coordinate of the current node
+    :return: false if point lies within any obstacle
+    """
+    # Check condition for out of bounds
+    if x <= 0 or x >= constants.MAP_SIZE[1] or y <= 0 or y >= constants.MAP_SIZE[0]:
+        return False
+    # Check condition to verify if point lies within obstacle
+    elif check_img[y, x].all() == 0:
+        return False
+
+    return True
+
+
+class Explorer:
+    def __init__(self, start_node: tuple, goal_node: tuple, method='d'):
+        """
+        Initialize the explorer with a start node and final goal node
+        :param start_node: a tuple of starting x-y coordinates provided by the user
+        :param goal_node: a tuple of goal coordinates provided by the user
+        :param method: method to explore the map - 'd' for Dijkstra & 'a' for A-star
+        """
+        # Store puzzle and goal nodes as class
+        self.start_node = start_node
+        self.goal_node = goal_node
+        self.method = method
+        # Define an empty list to store all generated nodes
+        self.generated_nodes = []
+        # Define 2-D arrays to store information about generated nodes and parent nodes
+        self.parent = np.full(fill_value=constants.NO_PARENT, shape=constants.MAP_SIZE)
+        # Define a 2-D array to store base cost of each node
+        self.base_cost = np.full(fill_value=constants.NO_PARENT, shape=constants.MAP_SIZE)
+        # Define video-writer of open-cv to record the exploration and final path
+        video_format = cv2.VideoWriter_fourcc('X', 'V', 'I', 'D')
+        self.video_output = cv2.VideoWriter('exploration_' + self.method + '.avi', video_format, 200.0,
+                                            (constants.MAP_SIZE[1], constants.MAP_SIZE[0]))
+
+    def get_heuristic_score(self, node: tuple) -> float:
+        """
+        Implement heuristic function for a-star by calculating manhattan distance
+        :param: node: tuple containing coordinates of the current node
+        :return: distance between the goal node and current node
+        """
+        # Evaluate euclidean distance between goal node and current node
+        return sqrt((self.goal_node[0] - node[0])**2 + (self.goal_node[1] - node[1])**2)
+
+    def get_final_weight(self, node: tuple, node_cost: float) -> float:
+        """
+        Get final weight for a-star
+        :param node: tuple containing coordinates of the current node
+        :param node_cost: cost of each node
+        :return: final weight for according to method
+        """
+        # If A-star is to be used
+        # Add heuristic value and node level to get the final weight for the current node
+        if self.method == 'a':
+            return self.get_heuristic_score(node) + node_cost
+        # Return cost for breadth-first search
+        elif self.method == 'b':
+            return constants.NODE_COST_BFS
+        # Return base cost if method being used is dijkstra
+        elif self.method == 'd':
+            return node_cost
+        # Print error statement and exit
+        print('Incorrect method! Please try again.')
+        quit()
+
+    def explore(self, map_img: np.ndarray) -> None:
+        """
+        Method to explore the map to find the goal
+        :param map_img: 2-d array with information of the map
+        :return: nothing
+        """
+        # Initialize priority queue
+        node_queue = PriorityQueue()
+        # Add cost-to-come of start node in the array
+        # Start node has a cost-to-come of 0
+        self.base_cost[self.start_node[0]][self.start_node[1]] = 0
+        self.generated_nodes.append(self.start_node)
+        self.parent[self.start_node[0]][self.start_node[1]] = constants.START_PARENT
+        # Add start node to priority queue
+        node_queue.put((self.get_final_weight(self.start_node, 0), self.start_node))
+        # Start exploring
+        while not node_queue.empty():
+            # Get node with minimum total cost
+            current_node = node_queue.get()
+            # Add node to generated nodes array
+            # Check for goal node
+            if current_node[1] == self.goal_node:
+                break
+            # Generate child nodes from current node
+            for i in range(constants.MAX_ACTIONS):
+                # Get coordinates of child node
+                y, x = take_action(i, current_node[1])
+                # Check whether child node is not within obstacle space and has not been previously generated
+                if (check_node_validity(map_img, x, constants.MAP_SIZE[0] - y) and
+                        self.parent[y][x] == constants.NO_PARENT):
+                    # Update cost-to-come of child node
+                    if i < 4:
+                        self.base_cost[y][x] = self.base_cost[current_node[1][0], current_node[1][1]] + 1
+                    else:
+                        self.base_cost[y][x] = self.base_cost[current_node[1][0], current_node[1][1]] + sqrt(2)
+                    # Add child node to priority queue
+                    # final_cost = self.get_final_weight((y, x), self.base_cost[y][x])
+                    node_queue.put((self.get_final_weight((y, x), self.base_cost[y][x]), (y, x)))
+                    self.generated_nodes.append((y, x))
+                    # Update parent of the child node
+                    self.parent[y][x] = np.ravel_multi_index([current_node[1][0], current_node[1][1]],
+                                                             dims=constants.MAP_SIZE)
+
+    def generate_path(self):
+        last_node = self.goal_node
+        path = []
+        while last_node != self.start_node:
+            path.append(last_node)
+            last_node = np.unravel_index(self.parent[last_node[0]][last_node[1]], shape=constants.MAP_SIZE)
+        path.append(self.start_node)
+        return path
+
+    def show_exploration(self, map_img: np.ndarray) -> None:
+        """
+        Show animation of map exploration and path from start to goal
+        :param map_img: 2-d array with information of the map
+        :return: nothing
+        """
+        blue = [255, 0, 0]
+        white = [200, 200, 200]
+        # Show all generated nodes
+        for y, x in self.generated_nodes:
+            map_img[constants.MAP_SIZE[0] - y, x] = white
+            self.video_output.write(map_img)
+        # Show path
+        data = self.generate_path()
+        for i in range(len(data) - 1, -1, -1):
+            map_img[constants.MAP_SIZE[0] - data[i][0], data[i][1]] = blue
+            self.video_output.write(map_img)
+        # Resize image to make it bigger and show it for 15 seconds
+        map_img = cv2.resize(map_img, (constants.MAP_SIZE[1] * 2, constants.MAP_SIZE[0] * 2))
+        for _ in range(10000):
+            self.video_output.write(map_img)
+        self.video_output.release()
+        cv2.destroyAllWindows()
